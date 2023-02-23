54e08a9cfe69322fb6de0393a01565452b97f2d7
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2023-02-24 00:35:40 +0530

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import json
 import random
+# from textblob import TextBlob
+
+
+
 
 bot_responses = open ("responses.json")  
 bot_data = json.load(bot_responses)
@@ -15,16 +19,28 @@ def bot_answer_fun(answer):
 #<<<<------------------------------------------
 
 #----------------------------------------------
-bot_greet = random.choice(bot_data["Intro"]["answer"])
-user_greet = user_data["Intro"]["answer"]
-# print(user_greet)
-#----------------------------------------------
+while True:
+    user_greet = (_.title() for _ in user_data["Intro"]["answer"])
+    bot_greet = random.choice(bot_data["Intro"]["answer"])
 
-user_que = input("User: ").title()
-print(user_que)
-if(user_que in user_greet):
-    bot_answer_fun(bot_greet)
+    user_question = ((_.replace("?","")).title() for _ in user_data["Questions"]["answer"])
+    bot_question = random.choice(bot_data["Questions"]["answer"])
+    # print(user_question)
+    #----------------------------------------------
 
+    user_que = input("\nUser: ").replace("?","").title()
+    # user_que = "I can't spel."
+    
+    # print(user_que)
+
+    if(user_que in user_greet):
+        bot_answer_fun(bot_greet)
+
+    elif(user_que in user_question):
+        bot_answer_fun(bot_question)
+    
+    else:
+        bot_answer_fun("I did'nt get that. Can you say it again?")
 
 
 
