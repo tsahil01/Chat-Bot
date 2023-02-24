dbec12a2a5ff69670028c6cadc3652d6759e897c
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2023-02-24 20:14:57 +0530

    third commit

--- main.py
+++ main.py
@@ -4,17 +4,28 @@ import random
 
 
 
-
 bot_responses = open ("responses.json")  
 bot_data = json.load(bot_responses)
 
 user_responses = open ("user_input.json")  
 user_data = json.load(user_responses)
 
+trainer = open ("trainer.json")  
+trainer_data = json.load(trainer)
+
+
+
+
 #----------------Function------------------>>>>
 
 def bot_answer_fun(answer):
     print(f"ChatBot: {answer}")
+    trainer_fn(user_que,answer)
+    
+def trainer_fn(que,ans):
+    trainer_data["user"][f"{que}"] = f"{ans}"
+    trainer = open('trainer.json', 'w') 
+    json.dump(trainer_data, trainer)
 
 #<<<<------------------------------------------
 
@@ -29,15 +40,18 @@ while True:
     #----------------------------------------------
 
     user_que = input("\nUser: ").replace("?","").title()
-    # user_que = "I can't spel."
-    
-    # print(user_que)
 
     if(user_que in user_greet):
         bot_answer_fun(bot_greet)
 
     elif(user_que in user_question):
         bot_answer_fun(bot_question)
+
+    elif("Remember" in user_que.split()):
+        x = (user_que.split())[1:]
+        x = ", ".join(x)
+        print(x)
+            
     
     else:
         bot_answer_fun("I did'nt get that. Can you say it again?")
